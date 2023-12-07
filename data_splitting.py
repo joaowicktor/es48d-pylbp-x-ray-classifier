b8cb1243f36457e7a0deef2a132d33c33f5bdc6b
João Wicktor <joaowicktor@gmail.com>
2023-12-06 21:19:00 -0300

    initial commit

---
+++ data_splitting.py
@@ -0,0 +1,30 @@
+import os
+import splitfolders
+
+# caminho das imagens
+input_path = './images_full/'
+output_path = './images_split/'
+
+def rename_images(path):
+    if os.path.exists(path):
+        for dirpath , dirnames , filenames in os.walk(path):
+            for index, file in enumerate(filenames):
+                full_path = os.path.join(dirpath,file)
+                extension = '.'+full_path.split('.')[-1] #-1 last element
+                folder_name = os.path.basename(dirpath)
+                newfilename = ''.join([folder_name+str(index), extension])
+                os.rename(full_path,os.path.join(dirpath,newfilename))
+
+if __name__ == '__main__':
+    # Create folders if they don't exist
+    os.makedirs(input_path, exist_ok=True)
+    os.makedirs(output_path, exist_ok=True)
+
+    rename_images(input_path)      
+    splitfolders.ratio(input=input_path, output=output_path,
+    seed=1337, ratio=(.8, .2), group_prefix=None, move=False) # default values
+    
+    # Rename val folder to test
+    os.rename(output_path+'val',output_path+'test')
+
+    print('[INFO] Splitting done!')
